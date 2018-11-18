04167f3dfee39366fb47a07046ad126e6f64b68b
Vadim <the.rocky.vadim@gmail.com>
2018-11-18 22:19:34 +0200

    Cleaning

--- KotlinAlter/src/main/kotlin/com/company/cntrl/Controller.kt
+++ KotlinAlter/src/main/kotlin/com/company/cntrl/Controller.kt
@@ -7,12 +7,11 @@ import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
 
 class Controller : KeyListener {
-    override fun keyTyped(keyEvent: KeyEvent?) {
 
-    }
+    override fun keyTyped(keyEvent: KeyEvent?) {}
 
     override fun keyPressed(keyEvent: KeyEvent?) {
-        val repainter: Repainter = SideFactory.getMoveSide(keyEvent!!.getKeyCode())
+        val repainter: Repainter = SideFactory.getMoveSide(keyEvent!!.keyCode)
         repainter.move(View.ball)
     }
 
